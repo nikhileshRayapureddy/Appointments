9039ecf1729a1baa9b3d93200b3562d78a610fcd
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2016-09-14 09:59:38 +0530

    c

---
+++ Appointments/BusinessLayer/BusinessObjects/SkillsBO.swift
@@ -0,0 +1,16 @@
+//
+//  SkillsBO.swift
+//  Appointments
+//
+//  Created by Kiran Kumar on 14/09/16.
+//  Copyright © 2016 NIKHILESH. All rights reserved.
+//
+
+import UIKit
+
+class SkillsBO: NSObject {
+    var strFirmId = ""
+    var strSkillId = ""
+    var strSkillName = ""
+    var strSkillNotes = ""
+}
